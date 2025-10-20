25c2d0060bb8737e4eefcb883da83738a56cbde4
Lenard <lenardpiir@gmail.com>
2025-10-20 16:43:49 +0300

    Initial project

---
+++ src/test/java/com/lhv/amlscreening/AmlScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhv.amlscreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmlScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
